42c9712099e05a408c2686410119ea0b2ac4f154
c-santana-sp <carlos.santana@bandtec.com.br>
2022-03-06 22:48:27 -0300

    feat: add create and get users modules

---
+++ lib/wabanex/users/create.ex
@@ -0,0 +1,13 @@
+defmodule Wabanex.Users.Create do
+
+  # alias Wabanex.User
+  # alias Wabanex.Repo
+  # or
+  alias Wabanex.{Repo, User}
+
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+  end
+end
