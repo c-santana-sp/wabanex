ad0f510df08cbca048257b74045b9171b9013db7
c-santana-sp <carlos.santana@bandtec.com.br>
2022-04-02 21:38:00 -0300

    fix: correcting @desc typo

--- lib/wabanex_web/schema/types/user.ex
+++ lib/wabanex_web/schema/types/user.ex
@@ -1,9 +1,9 @@
 defmodule WabanexWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
 
-  import_types WabanexWeb.Schema.Types.Custom.UUID4
+  # import_types WabanexWeb.Schema.Types.Custom.UUID4
 
-  @desc "Logi user representation"
+  @desc "Logic user representation"
   object :user do
     field :id, non_null(:uuid4)
     field :name, non_null(:string)
